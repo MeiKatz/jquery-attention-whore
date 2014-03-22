3c41d99f311d9ba41a3fcdd796c234a02852100c
meikatz <gregor.mitzka@gmail.com>
2014-03-22 11:40:05 +0100

    minor changes

--- README.md
+++ README.md
@@ -12,15 +12,15 @@ Just include the following line after you've include the jQuery library:
 Make her handsome
 ----------------
 We know already that your application is a little attention whore. Let her show how annoying she could be.
-```
-# normal call
+```JavaScript
+// normal call
 $.theAttentionWhore.attract( "I want your attention!" );
 
-# set her level of annoyingness (how often the title will change)
-# -- change title every 500 milliseconds
+// set her level of annoyingness (how often the title will change)
+// -- change title every 500 milliseconds
 $.theAttentionWhore.attract( "I want your attention twice a second!", 500 );
 
-# make her shut up if she gets to annoying
+// make her shut up if she gets to annoying
 $.theAttentionWhore.shutup();
 ```
 You don't have to bother if she changes her news - you won't lose your actual title.
--- jquery.attention-whore.js
+++ jquery.attention-whore.js
@@ -1,11 +1,23 @@
+  /*
+   * LICENSE: "THE BEER-WARE LICENSE" (Revision 42)
+   * <gregor.mitzka@gmail.com> wrote this file. As long as you retain this notice you
+   * can do whatever you want with this stuff. If we meet some day, and you think
+   * this stuff is worth it, you can buy me a beer in return Gregor Mitzka
+   * 
+   * @package      jQuery Attention Whore
+   * @author       Gregor Mitzka <gregor.mitzka@gmail.com>
+   * @copyright    2014 (C) Gregor Mitzka
+   * @version      1.0
+   * @license      The Beer-Ware License
+   */
 (function( $ ) {
   var original_title,
       $title   = $( "title" ).first();
       interval = null;
  
   var start = function ( notification_message, duration ) {
-  	duration = duration || 1000;
-  	
+    duration = duration || 1000;
+    
     stop();
     original_title = $title.html();
   
@@ -19,16 +31,16 @@
   };
   
   var stop = function() {
-  	if ( interval !== null ) {
-  		window.clearInterval( interval );
-  		$title.html( original_title );
-  	}
+    if ( interval !== null ) {
+      window.clearInterval( interval );
+      $title.html( original_title );
+    }
   };
   
   $( window ).on( "focus.attention-whore", stop );
  
- 	$.theAttentionWhore = {
- 		"attract" : start,
- 		"shutup"  : stop
- 	};
+   $.theAttentionWhore = {
+     "attract" : start,
+     "shutup"  : stop
+   };
 })( jQuery );
